449e9832c11ae3893f4f555f4731829a2a9d7858
austinlang748 <74215480+austinlang748@users.noreply.github.com>
2021-01-30 16:09:50 -0700

    created velocity.h

---
+++ velocity.h
@@ -0,0 +1,33 @@
+/************************************************
+* Velocity Class
+* Written by Austin Hilderbrand
+*************************************************/
+
+class Velocity
+{
+public:
+   Velocity(double dx_init = 0.0, double dy_init = 0.00)
+   {
+      dx = dx_init;
+      dy = dy_init;
+   }
+
+   double getDX()
+   {
+      return dx;
+   }
+
+   double getDY()
+   {
+      return dy;
+   }
+
+   double getSpeed()
+   {
+      return sqrt()
+   }
+
+private:
+   double dx;
+   double dy;
+};
